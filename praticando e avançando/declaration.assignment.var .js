425185ae12d8d16a05f9f6bedf3b3de1af5405e1
Bruno Filadelfo <Bruno.Filadelfo@Outlook.Com>
2023-02-05 19:18:16 -0300

    primeiro commit

---
+++ praticando e avançando/declaration.assignment.var .js
@@ -0,0 +1,38 @@
+/* 01 
+
+  // variáveis e tipos de dados 
+  // declaração or decla
+
+*/
+
+var name 
+
+/* 02 
+
+   // assignment or atribuição de valores 
+
+*/
+
+ var name = "Jon"
+
+/* 03 
+
+   // que tipo de dado foi colocado na variável
+
+*/
+
+console.log(typeof name)
+
+
+/* 04 
+
+   // agrupamento de declarações 
+
+*/ 
+
+let age, isHuman 
+
+age = 18 
+isHuman = true 
+
+console.log(name, age, isHuman)
