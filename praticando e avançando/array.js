d94cf5c6bff8e123c8250d6adc5295a6cf35ac78
Bruno Filadelfo <Bruno.Filadelfo@Outlook.Com>
2023-02-10 13:40:15 -0300

    alteração do javascript

---
+++ praticando e avançando/array.js
@@ -0,0 +1,22 @@
+/* 01
+
+  // Array 
+
+*/ 
+
+  const carros = [
+    'Toyota', //0
+    'Chevrolet', //1
+    'Mercedes benz', //2
+
+    {
+        name: 'Fiat', //objeto  //4
+        age: 5 
+    }
+  ]
+
+  // acessar valores depois do array
+
+  console.log(carros)
+  console.log(carros[3].name)
+  
---
+++ praticando e avançando/exercicios.js
@@ -0,0 +1,101 @@
+// 1. Declare uma variável de nome weight 
+//let weight
+
+// 2. Que tipo de dado é a variáveç acima ? 
+//console.log(typeof weight) //
+
+/* 
+    3. Declare uma variável e atribua valores para caa um dos dados:
+      * name: string
+      * age: number (integer)
+      * stars: number (float)
+      * isSubscribed: Boolean
+*/
+
+//let name = "Jon"
+//let age = 26
+//weight = 4.8
+//let isSubscribed = true
+
+/* 4. A variável student abaixo é de que tipo de dados ? 
+
+   4.1 Atribua a ela as mesmas propriedades e valores do exercício 3.
+
+   4.2 Mostre o console a seguinte mensagem:
+   
+   <name> de idade <age> pesa <weight> kg.
+
+    atenção substitua <name> <age> e <weight> pelos valores de cada propriedadesdo objeto
+    
+*/
+
+let student = {
+    name: "Jon",
+    age:26,
+    weight: 4.8,
+    isSubscribed: true,
+}
+
+//console.log(`${students.name} de idade ${students.age} pesa ${students.weight} kg.`)
+    
+/*
+   5. Declare uma variável do tipo array, de nome students e atribua a ela nenhum valor, ou seja, somente o array vazio
+*/
+
+let students = []
+
+
+/* 
+    6. Reatribua valor para a variável acima, cilocando dentro dela o objeto student da questão 4. (Não copiar e colar o objeto,
+     mas usar o objeto criado e inserir ela no array)
+*/
+
+students = [
+    students
+]
+
+console.log(students)
+
+/* 
+    7. Coloque no console o valor da posição zero do Array acima
+*/
+
+//console.log(students[0])
+
+
+
+/*
+    8. Crie um novo student e coloque na posição 1 do array students  
+*/
+
+const Jon = { 
+    name: "Jon",
+    age: 26,
+    weight: 74.8,
+    isSubscribed: true,
+}
+
+students = [
+    students,
+    Jon
+]
+
+console.log(students)
+
+
+
+/* 
+    9. Sem rodar o código responda qual é a resposta do código abaixo e por que?
+      Após sua resposta, rode o código e veja se você acertou
+      
+      console.log(a)
+      var a = 1 
+*/
+
+console.log(a)
+var a = 1
+
+
+
+ 
+
