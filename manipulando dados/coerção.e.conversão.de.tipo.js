81e561c67e777fdc78cb67b262721db6f921d9c4
Bruno Filadelfo <Bruno.Filadelfo@Outlook.Com>
2023-02-12 04:39:37 -0300

    manipulando dados js

---
+++ manipulando dados/coerção.e.conversão.de.tipo.js
@@ -0,0 +1,10 @@
+/* 
+
+     type conversion (typecasting) vs Type coersion
+
+     * Alteração de um tipo de dado para outro tipo
+
+
+*/
+
+console.log(Number ('9') + 5)
---
+++ manipulando dados/contando.caracteres.e.digitos.js
@@ -0,0 +1,8 @@
+// Manipulando Strings e Números
+
+// COntar quantos caracters tem uma palavra e quantos dígitos tem um número
+
+let word = "Paralelepipedo"
+console.log(word.length)
+let number = 1234
+console.log(String(number).length)
