aa03150a740e011ba2d6b5b8ac7c321a20b2ad6d
Bruno Filadelfo <Bruno.Filadelfo@Outlook.Com>
2023-02-20 04:09:59 -0300

    operadores de comparação

---
+++ expressões e operadores/operadores.aritimeticos.js
@@ -0,0 +1,44 @@
+// Operadores Aritiméticos 
+
+// multiplicação * 
+// console.log(3.2 * 5.5)
+
+// divisão /
+//console.log(12 / 2) 
+
+// soma +
+//console.log(34 + 67)
+
+// subtração -
+
+//console.log(85 - 65)
+
+// resto de divisão %
+//let remainder 
+//remainder = 15 % 13
+//console.log(remainder)
+
+// incremento ++ 
+//let increment = 0
+//increment++ 
+//increment++
+//console.log(increment)
+
+// incremento ++ 
+//let increment = 0
+//console.log(++increment)
+//console.log(increment)
+
+// decremento --
+//let decrement = 0
+//console.log(--decrement)
+//console.log(decrement)
+
+// exponencial **
+//console.log(3 ** 3)
+
+
+
+
+
+
---
+++ expressões e operadores/typeof.delete.js
@@ -0,0 +1,12 @@
+/* 
+    Operadores unários
+    typeof
+    delete
+*/
+
+const person = {
+    name: 'John',
+    age: 26,
+}
+delete person.age
+console.log(person)
